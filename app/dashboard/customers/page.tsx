969f63663f26d57ef63bb39f4269380ed32431b6
Subeshsingh <subeshbikkisingh@gmail.com>
2025-02-05 14:42:16 +0530

    Added database PostGre Neon, implemented dynamic server side rendering and streaming. Added skelton for shwoing loading state.

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 
-export default function page() {
+export default async function page() {
+  await new Promise((resolve) => setTimeout(resolve, 3000));
   return <div>Customers Page</div>;
 }
